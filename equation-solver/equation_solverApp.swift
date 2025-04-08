21c9f4661ea448a49f3ccea0cf8cd946189c8862
Daniela Garcia <158435403+danielagarcia80@users.noreply.github.com>
2025-04-08 15:01:08 -0700

    Add xcode files

---
+++ equation-solver/equation_solverApp.swift
@@ -0,0 +1,17 @@
+//
+//  equation_solverApp.swift
+//  equation-solver
+//
+//  Created by Daniela Garcia on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct equation_solverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
